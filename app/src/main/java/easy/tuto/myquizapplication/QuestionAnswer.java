db0896c9efae6cd363f98cfa59c07a3e398323fb
nimitagarwal <101705195+agarwalnimit@users.noreply.github.com>
2023-05-17 22:31:38 +0530

    3 ques created

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "In which directory XML layout files are stored?",
+            "Which programming language is used for android application development?",
+            "What does API stand for?"
+    };
+
+    public static String choices[][]={
+            {"/assets","/src","/res/values","/res/layout"},
+            {"Java","PHP","JSX","NodeJS"},
+            {"Algorithmic Protocol Interface","Accelerated Programming Interface","Application Programming Interface","None of above"}
+
+    };
+
+    public static String correctAnswers[]={
+            "/res/layout",
+            "Java",
+            "Application Programming Interface"
+
+    };
+
+}
