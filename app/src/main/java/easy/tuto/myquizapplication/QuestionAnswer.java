b2696767378d6de1c12df07e03ac5123ca2e1a62
nimitagarwal <101705195+agarwalnimit@users.noreply.github.com>
2023-05-17 23:50:34 +0530

    ques added

--- app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -5,20 +5,23 @@ public class QuestionAnswer {
     public static String question[]={
             "In which directory XML layout files are stored?",
             "Which programming language is used for android application development?",
-            "What does API stand for?"
+            "What does API stand for?",
+            "Which one of the following is not a Java feature?"
     };
 
     public static String choices[][]={
             {"/assets","/src","/res/values","/res/layout"},
             {"Java","PHP","JSX","NodeJS"},
-            {"Algorithmic Protocol Interface","Accelerated Programming Interface","Application Programming Interface","None of above"}
+            {"Algorithmic Protocol Interface","Accelerated Programming Interface","Application Programming Interface","None of above"},
+            {"Object-oriented","Use of pointers","Portable","Dynamic and Extensible"}
 
     };
 
     public static String correctAnswers[]={
             "/res/layout",
             "Java",
-            "Application Programming Interface"
+            "Application Programming Interface",
+            "Use of pointers"
 
     };
 
